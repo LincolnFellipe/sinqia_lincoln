91168fea407077114ea4b940fe1d796a5e63dbbc
Lincoln Souza <lincolnfellipe171@gmail.com>
2019-09-04 16:19:16 -0300

    Arquivo html e Javascript criado

---
+++ script.js
@@ -0,0 +1,13 @@
+function funcao1()
+{
+alert("Biscoito não existe, sempre foi BOLACHA");
+}
+
+function funcao2()
+{
+alert("Nescau >>>>> Toddy");
+}
+function funcao3()
+{
+alert("Pão de queijo é ruim");
+}
